01302d3d3104e4632afb18b02e74b7fb443964ea
yihang6120 <yihang6120@gmail.com>
2025-05-02 20:24:17 +0800

    read item generate menu

--- Assignment/FrmOrder.Designer.vb
+++ Assignment/FrmOrder.Designer.vb
@@ -23,35 +23,80 @@ Partial Class FrmOrder
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.grpCart = New System.Windows.Forms.GroupBox()
+        Me.lblTotalAmount = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dgvOrder = New System.Windows.Forms.DataGridView()
+        Me.lblTableNo = New System.Windows.Forms.Label()
         Me.lblOrderItem = New System.Windows.Forms.Label()
         Me.label1 = New System.Windows.Forms.Label()
-        Me.lblTableNo = New System.Windows.Forms.Label()
+        Me.flpMenu = New System.Windows.Forms.FlowLayoutPanel()
+        Me.cmbCategory = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.grpCart.SuspendLayout()
+        CType(Me.dgvOrder, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'grpCart
         '
         Me.grpCart.BackColor = System.Drawing.SystemColors.Control
+        Me.grpCart.Controls.Add(Me.lblTotalAmount)
+        Me.grpCart.Controls.Add(Me.Label2)
+        Me.grpCart.Controls.Add(Me.dgvOrder)
         Me.grpCart.Controls.Add(Me.lblTableNo)
         Me.grpCart.Controls.Add(Me.lblOrderItem)
         Me.grpCart.Controls.Add(Me.label1)
         Me.grpCart.Font = New System.Drawing.Font("Yu Gothic UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.grpCart.Location = New System.Drawing.Point(25, 11)
+        Me.grpCart.Location = New System.Drawing.Point(28, 14)
         Me.grpCart.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.grpCart.Name = "grpCart"
         Me.grpCart.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.grpCart.Size = New System.Drawing.Size(461, 516)
+        Me.grpCart.Size = New System.Drawing.Size(519, 645)
         Me.grpCart.TabIndex = 1
         Me.grpCart.TabStop = False
         Me.grpCart.Text = "Cart"
+        '
+        'lblTotalAmount
+        '
+        Me.lblTotalAmount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalAmount.Location = New System.Drawing.Point(195, 562)
+        Me.lblTotalAmount.Name = "lblTotalAmount"
+        Me.lblTotalAmount.Size = New System.Drawing.Size(182, 37)
+        Me.lblTotalAmount.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(25, 562)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(176, 37)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Total Amount : "
+        '
+        'dgvOrder
+        '
+        Me.dgvOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvOrder.Location = New System.Drawing.Point(25, 126)
+        Me.dgvOrder.Name = "dgvOrder"
+        Me.dgvOrder.RowHeadersWidth = 62
+        Me.dgvOrder.RowTemplate.Height = 28
+        Me.dgvOrder.Size = New System.Drawing.Size(463, 400)
+        Me.dgvOrder.TabIndex = 4
+        '
+        'lblTableNo
+        '
+        Me.lblTableNo.Font = New System.Drawing.Font("Yu Gothic UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTableNo.Location = New System.Drawing.Point(141, 40)
+        Me.lblTableNo.Name = "lblTableNo"
+        Me.lblTableNo.Size = New System.Drawing.Size(155, 38)
+        Me.lblTableNo.TabIndex = 3
+        Me.lblTableNo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'lblOrderItem
         '
         Me.lblOrderItem.AutoSize = True
         Me.lblOrderItem.Font = New System.Drawing.Font("Yu Gothic UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblOrderItem.Location = New System.Drawing.Point(18, 70)
+        Me.lblOrderItem.Location = New System.Drawing.Point(20, 88)
         Me.lblOrderItem.Name = "lblOrderItem"
-        Me.lblOrderItem.Size = New System.Drawing.Size(103, 23)
+        Me.lblOrderItem.Size = New System.Drawing.Size(125, 30)
         Me.lblOrderItem.TabIndex = 2
         Me.lblOrderItem.Text = "Order Item :"
         '
@@ -59,32 +104,51 @@ Partial Class FrmOrder
         '
         Me.label1.AutoSize = True
         Me.label1.Font = New System.Drawing.Font("Yu Gothic UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.label1.Location = New System.Drawing.Point(35, 35)
+        Me.label1.Location = New System.Drawing.Point(39, 44)
         Me.label1.Name = "label1"
-        Me.label1.Size = New System.Drawing.Size(86, 23)
+        Me.label1.Size = New System.Drawing.Size(106, 30)
         Me.label1.TabIndex = 0
         Me.label1.Text = "Table No :"
         Me.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
-        'lblTableNo
+        'flpMenu
         '
-        Me.lblTableNo.Font = New System.Drawing.Font("Yu Gothic UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTableNo.Location = New System.Drawing.Point(125, 32)
-        Me.lblTableNo.Name = "lblTableNo"
-        Me.lblTableNo.Size = New System.Drawing.Size(138, 30)
-        Me.lblTableNo.TabIndex = 3
-        Me.lblTableNo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.flpMenu.Location = New System.Drawing.Point(553, 54)
+        Me.flpMenu.Name = "flpMenu"
+        Me.flpMenu.Size = New System.Drawing.Size(576, 509)
+        Me.flpMenu.TabIndex = 2
+        '
+        'cmbCategory
+        '
+        Me.cmbCategory.FormattingEnabled = True
+        Me.cmbCategory.Location = New System.Drawing.Point(654, 6)
+        Me.cmbCategory.Name = "cmbCategory"
+        Me.cmbCategory.Size = New System.Drawing.Size(121, 28)
+        Me.cmbCategory.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(553, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 25)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Category : "
         '
         'FrmOrder
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1022, 559)
+        Me.ClientSize = New System.Drawing.Size(1150, 699)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.cmbCategory)
+        Me.Controls.Add(Me.flpMenu)
         Me.Controls.Add(Me.grpCart)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "FrmOrder"
         Me.Text = "Order Page"
         Me.grpCart.ResumeLayout(False)
         Me.grpCart.PerformLayout()
+        CType(Me.dgvOrder, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -93,4 +157,10 @@ Partial Class FrmOrder
     Friend WithEvents lblOrderItem As Label
     Friend WithEvents label1 As Label
     Friend WithEvents lblTableNo As Label
+    Friend WithEvents lblTotalAmount As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents dgvOrder As DataGridView
+    Friend WithEvents flpMenu As FlowLayoutPanel
+    Friend WithEvents cmbCategory As ComboBox
+    Friend WithEvents Label3 As Label
 End Class
--- Assignment/FrmOrder.vb
+++ Assignment/FrmOrder.vb
@@ -1,5 +1,102 @@
-﻿Public Class FrmOrder
+﻿Imports System.Data.Linq
+Imports System.Data.SqlClient
+
+Public Class FrmOrder
     Private Sub grpCart_Enter(sender As Object, e As EventArgs) Handles grpCart.Enter
 
     End Sub
+
+    Private Sub FrmOrder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeDataGridView()
+        LoadCategories()
+    End Sub
+
+    Private Sub LoadCategories()
+        Dim db As New BL_farizDataContext()
+
+        Dim categories = (From i In db.Items
+                          Select i.Item_Category
+                          Distinct).ToList()
+
+        cmbCategory.DataSource = categories
+        cmbCategory.SelectedIndex = -1
+    End Sub
+
+
+    Private Sub cmbCategory_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbCategory.SelectedIndexChanged
+        If cmbCategory.SelectedIndex <> -1 Then
+            LoadItemsByCategory(cmbCategory.SelectedItem.ToString())
+        End If
+    End Sub
+
+    Private Sub LoadItemsByCategory(category As String)
+        Dim db As New BL_farizDataContext()
+        flpMenu.Controls.Clear()
+
+        Dim items = From i In db.Items
+                    Where i.Item_Category = category And i.Item_Quantity > 0
+                    Select i
+
+        For Each item In items
+            Dim itemPanel As New Panel With {
+            .Width = 120,
+            .Height = 120,
+            .BorderStyle = BorderStyle.FixedSingle,
+            .Tag = item.Item_Id
+        }
+
+            Dim lblName As New Label With {
+            .Text = item.Item_Name,
+            .AutoSize = True,
+            .Location = New Point(10, 10)
+        }
+
+            Dim lblPrice As New Label With {
+            .Text = "RM " & item.Item_Price.ToString("F2"),
+            .AutoSize = True,
+            .Location = New Point(10, lblName.Bottom + 5)
+        }
+
+            Dim btnAdd As New Button With {
+            .Text = "Add",
+            .Width = 60,
+            .Location = New Point(10, lblPrice.Bottom + 10)
+        }
+
+            'AddHandler btnAdd.Click, Sub(s, e)
+            '                             AddToOrder(item.Item_Id)
+            '                         End Sub
+
+            itemPanel.Controls.Add(lblName)
+            itemPanel.Controls.Add(lblPrice)
+            itemPanel.Controls.Add(btnAdd)
+
+            flpMenu.Controls.Add(itemPanel)
+        Next
+    End Sub
+
+    Private Sub InitializeDataGridView()
+        If dgvOrder.Columns.Count = 0 Then
+            dgvOrder.Columns.Add("Item_Id", "Item ID")
+            dgvOrder.Columns.Add("Item_Name", "Item Name")
+            dgvOrder.Columns.Add("Quantity", "Quantity")
+            dgvOrder.Columns.Add("Item_Price", "Price")
+            dgvOrder.Columns.Add("SubTotal", "SubTotal")
+
+            dgvOrder.Columns("Item_Price").ValueType = GetType(Decimal)
+            dgvOrder.Columns("SubTotal").ValueType = GetType(Decimal)
+        End If
+    End Sub
+
+
+    Private Sub UpdateTotal()
+        Dim total As Decimal = 0
+
+        For Each row As DataGridViewRow In dgvOrder.Rows
+            total += Convert.ToDecimal(row.Cells("SubTotal").Value)
+        Next
+
+        lblTotalAmount.Text = "Total: RM " & total.ToString("F2")
+    End Sub
+
 End Class
