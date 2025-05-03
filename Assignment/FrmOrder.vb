076ab392b1028b5a50403cbb3702609448692d49
yihang6120 <yihang6120@gmail.com>
2025-05-03 11:42:41 +0800

    modify some UI, table button and back button at order page

--- Assignment/FrmOrder.vb
+++ Assignment/FrmOrder.vb
@@ -303,4 +303,8 @@ Public Class FrmOrder
         End Try
     End Sub
 
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Me.Close()
+
+    End Sub
 End Class
